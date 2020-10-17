6cf4235708780127f3bd689a1920387c78198ee8
Chrisdigity <chrisdigity@gmail.com>
2020-10-17 13:41:27 +1000

    cleanup

--- index.js
+++ index.js
@@ -34,17 +34,84 @@ const CHIDS_NOINTRO = [
 /* required modules */
 const FS = require("fs")
 const Path = require("path")
+const YTDL = require("ytdl-core")
 const Discord = require("discord.js")
-const BOT = require("./mybots.js")
-const Zyborg = new Discord.Client()
-const ZJ_Chillstep = new BOT.YTMusic('ZJChillstep', CHID_CHILLSTEP, LINK_CHILLSTEP)
-const ZJ_NCM = new BOT.YTMusic('ZJNoCopyrightMusic', CHID_NCM, LINK_NCM)
-const ZJ_NCS = new BOT.YTMusic('ZJNoCopyrightSounds', CHID_NCS, LINK_NCS)
+
+/* YTMusic constructor */
+const VOLUME = 0.2
+const YTMusic = function(n, id, src) {
+  // make 'this' reliably accessible
+  const _self = this
+  
+  // define options and parameters
+  _self.name = n
+  _self.chid = id
+  _self.source = src
+  _self.conn = null
+  _self.count = 0
+  // define the client
+  _self.client = new Discord.Client()
+  
+  // setup events for the client...
+  /* ...on ready, log event and post stream link */
+  _self.client.on("ready", () => {
+    console.log(`${_self.client.user.tag} is ready...`)
+  })
+  /* ...on voiceStateUpdate, check user joined before starting */
+  _self.client.on("voiceStateUpdate", (old, cur) => {
+    if(old.channelID != _self.chid && cur.channelID == _self.chid)
+    {
+      // user joined Chillstep ++increment count
+      if(++_self.count == 1) {
+        // start chillstep bot
+        cur.member.voice.channel.join().then(connection => {
+          _self.conn = connection
+          connection.play(
+            YTDL(_self.source, {quality:'highestaudio'}),
+            {volume: VOLUME}
+          ).on("error", error => {
+            console.error(`${_self.name}: ${error}`)
+            connection.disconnect()
+            _self.conn = null
+          })
+        }).catch(console.error)
+      }
+    }
+    else if(old.channelID == _self.chid && cur.channelID != _self.chid)
+    {
+      // user exited Chillstep --decrement count
+      if(--_self.count < 0)
+        _self.count = 0
+      // check for lonely bot
+      if(_self.count <= 1) {
+        if(_self.conn) {
+          // remove chillstep bot
+          _self.conn.disconnect()
+          _self.conn = null
+        }
+      }
+    }
+  })
+  
+  _self.destroy = function() {
+    _self.client.destroy()
+  }
+  
+  _self.login = function(token) {
+    _self.client.login(token)
+  }
+}
 
 /* Voice connection and dispatcher containers */
 let Vconn = null
 let Vqueue = []
 let Vcooldown = {}
+
+/* Initialize discord bots */
+const Zyborg = new Discord.Client()
+const ZJ_Chillstep = new YTMusic('ZJ_Chillstep', CHID_CHILLSTEP, LINK_CHILLSTEP)
+const ZJ_NCM = new YTMusic('ZJ_NoCopyrightMusic', CHID_NCM, LINK_NCM)
+const ZJ_NCS = new YTMusic('ZJ_NoCopyrightSounds', CHID_NCS, LINK_NCS)
 
 /* ZYBORG function to clear spam channel every ~24 hours */
 const CLEAR_SPAM = function() {
