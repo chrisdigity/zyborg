dce3c50b053bb8903a2b4da75e4ed93137d6a604
Chrisdigity <chrisdigity@gmail.com>
2020-10-17 12:42:23 +1000

    discord bot overhaul

--- index.js
+++ index.js
@@ -8,46 +8,38 @@ require("dotenv").config()
  * USER CONFIGURATION *
  **********************/
 
+/* Youtube Music Links */
+const LINK_CHILLSTEP = 'https://www.youtube.com/watch?v=DLaV_7vwiN8'
+const LINK_NCM = 'https://www.youtube.com/watch?v=Oxj2EAr256Y'
+const LINK_NCS = 'https://www.youtube.com/watch?v=cQKuD49zKvU'
+
 /* Channel IDs*/
 const CHID_SPAM = '675644867447095296'
 const CHID_SERVER = '651364689665720350'
 const CHID_PRESENCE = '730596136938635334'
 const CHID_VOICE = '720459302963380274'
-const CHID_A2C = '651366291160170516'    //voice
-const CHID_FOCUS = '651374941840867338'  //voice
-const CHID_ANIME = '730931465793044550'  //voice
-const CHID_LINKS = '765706158169391114'        //muzix(text)
-const CHID_CHILLSTEP = '725473321868591104'    //muzix
-const CHID_NOCOPYRIGHT = '766768566263087124'  //muzix
+const CHID_STORY = '651366291160170516' //voice
+const CHID_ANIME = '730931465793044550' //voice
+const CHID_CHILLSTEP = '725473321868591104' //muzix
+const CHID_NCM = '766768566263087124' //muzix
+const CHID_NCS = '766835115564859403' //muzix
 const CHIDS_NOINTRO = [
-   CHID_A2C, CHID_FOCUS, CHID_ANIME, CHID_CHILLSTEP, CHID_NOCOPYRIGHT,
+   CHID_STORY, CHID_ANIME, CHID_CHILLSTEP, CHID_NCM, CHID_NCS,
 ]
 
 /**************************
  * END USER CONFIGURATION *
  **************************/
 
-
-/* CONSTANTS */
-const YT_VOLUME = 0.2
-const MINUTES10 = 10 * 60 * 1000
-
 /* required modules */
 const FS = require("fs")
 const Path = require("path")
-const YTDL = require("ytdl-core")
 const Discord = require("discord.js")
+const Musicbot = require("./musicbot.js")
 const Zyborg = new Discord.Client()
-const ZJChillstep = new Discord.Client()
-const ZJNoCopyright = new Discord.Client()
-
-/* muzix data */
-const ZJChillstep_link = 'https://www.youtube.com/watch?v=DLaV_7vwiN8'
-const ZJNoCopyright_link = 'https://www.youtube.com/watch?v=Oxj2EAr256Y'
-let ZJChillstep_conn = null
-let ZJNoCopyright_conn = null
-let ZJChillstep_count = 0
-let ZJNoCopyright_count = 0
+const ZJ_Chillstep = new Musicbot('ZJChillstep', CHID_CHILLSTEP, LINK_CHILLSTEP)
+const ZJ_NCM = new Musicbot('ZJNoCopyrightMusic', CHID_NCM, LINK_NCM)
+const ZJ_NCS = new Musicbot('ZJNoCopyrightSounds', CHID_NCS, LINK_NCS)
 
 /* Voice connection and dispatcher containers */
 let Vconn = null
@@ -101,22 +93,6 @@ const PLAY_NEXT_INTRO = function(connection) {
 Zyborg.on("ready", () => {
   console.log(`${Zyborg.user.tag} is ready...`)
   CLEAR_SPAM()
-  
-  // clear livestream links channel and start music bots
-  Zyborg.channels.fetch(CHID_LINKS).then(channel => {
-    channel.messages.fetch().then(messages => {
-      if(messages.array().length) {
-        messages.each(message => {
-          message.delete().then(msg => {
-            console.log(`Deleted ${msg.channel.name} message, ID#${msg.id}...`)
-          }).catch(console.error)
-        })
-      }
-      // start music bots
-      ZJChillstep.login(process.env.ZJCHILLSTEP_TOKEN)
-      ZJNoCopyright.login(process.env.ZJNOCOPYRIGHT_TOKEN)
-    }).catch(console.error)
-  }).catch(console.error)
 })
 Zyborg.on("message", message => {
   if(message.channel.id != CHID_SPAM || !message.member.hasPermission('ADMINISTRATOR'))
@@ -191,122 +167,25 @@ Zyborg.on("voiceStateUpdate", (old, cur) => {
     * - DOES NOT PLAY in CHIDS_NOINTRO channels
     * - DOES NOT PLAY if there is only one (1) other person in the channel
     * - DOES NOT PLAY if user ID is still in cooldown
-    * - DOES NOT PLAY if no sound file exists for user ID */
+    * - DOES NOT PLAY if no sound file exists for user ID 
    if(action == 'left') {
       Vcooldown[cur.id] = Date.now()
    } else if(action == 'joined' &&
              !CHIDS_NOINTRO.includes(cur.channelID) &&
              cur.channel.members.array().length > 2 &&
-             (!Vcooldown.hasOwnProperty(cur.id) || Vcooldown[cur.id] < Date.now() - MINUTES10)) {
-      /* check file exists */
+             !Vcooldown.hasOwnProperty(cur.id)) {
+      /* check file exists *
       let mp3file = Path.join(__dirname, `./sound/${cur.id}.mp3`)
       if(FS.existsSync(mp3file)){
-         /* queue intro sound */
+         /* queue intro sound *
          Vqueue.push(mp3file)
-         /* join channel and/or play intro */
+         /* join channel and/or play intro *
          if(!Vconn)
             cur.member.voice.channel.join().then(PLAY_NEXT_INTRO).catch(console.error)
       }
-   }
+   }*/
 })
 
-
-/********************************/
-/* Begin ZJChillstep events... */
-
-/* ...on ready, log event and post livestream link */
-ZJChillstep.on("ready", () => {
-  console.log(`${ZJChillstep.user.tag} is ready...`)
-  ZJChillstep.channels.fetch(CHID_LINKS).then(channel => {
-    channel.send(
-      `Streaming Chillstep live on <#${CHID_CHILLSTEP}>\n${ZJChillstep_link}`
-    ).catch(console.error)
-  }).catch(console.error)
-})
-/* ...on voiceStateUpdate, check appropriate music channel */
-ZJChillstep.on("voiceStateUpdate", (old, cur) => {
-  if(old.channelID != CHID_CHILLSTEP && cur.channelID == CHID_CHILLSTEP) {
-    // user joined Chillstep ++increment count
-    if(++ZJChillstep_count == 1) {
-      // start chillstep bot
-      cur.member.voice.channel.join().then(connection => {
-        ZJChillstep_conn = connection
-        connection.play(
-          YTDL(ZJChillstep_link, {quality:'highestaudio'}),
-          {volume: YT_VOLUME}
-        ).on("error", error => {
-          console.error(`ZJChillstep: ${error}`)
-          ZJChillstep.channels.fetch(CHID_SPAM).then(channel => {
-            channel.send(`${error}\n*Please try again later...*`).catch(console.error)
-            connection.disconnect()
-            ZJChillstep_conn = null
-          }).catch(console.error)
-        })
-      }).catch(console.error)
-    }
-  } else if(old.channelID == CHID_CHILLSTEP && cur.channelID != CHID_CHILLSTEP) {
-    // user exited Chillstep --decrement count
-    if(--ZJChillstep_count < 0)
-      ZJChillstep_count = 0
-    // check for lonely bot
-    if(ZJChillstep_count <= 1) {
-      if(ZJChillstep_conn) {
-        // remove chillstep bot
-        ZJChillstep_conn.disconnect()
-        ZJChillstep_conn = null
-      }
-    }
-  }
-})
-
-
-/********************************/
-/* Begin ZJNoCopyright events... */
-
-/* ...on ready, log event and post livestream link */
-ZJNoCopyright.on("ready", () => {
-  console.log(`${ZJNoCopyright.user.tag} is ready...`)
-  ZJNoCopyright.channels.fetch(CHID_LINKS).then(channel => {
-    channel.send(
-      `Streaming NoCopyrightMusic live on <#${CHID_NOCOPYRIGHT}>\n${ZJNoCopyright_link}`
-    ).catch(console.error)
-  }).catch(console.error)
-})
-/* ...on voiceStateUpdate, check appropriate music channel */
-ZJNoCopyright.on("voiceStateUpdate", (old, cur) => {
-  if(old.channelID != CHID_NOCOPYRIGHT && cur.channelID == CHID_NOCOPYRIGHT) {
-    // user joined Chillstep ++increment count
-    if(++ZJNoCopyright_count == 1) {
-      // start chillstep bot
-      cur.member.voice.channel.join().then(connection => {
-        ZJNoCopyright_conn = connection
-        connection.play(
-          YTDL(ZJNoCopyright_link, {quality:'highestaudio'}),
-          {volume: YT_VOLUME}
-        ).on("error", error => {
-          console.error(`ZJNoCopyright: ${error}`)
-          ZJNoCopyright.channels.fetch(CHID_SPAM).then(channel => {
-            channel.send(`${error}\n*Please try again later...*`).catch(console.error)
-            connection.disconnect()
-            ZJNoCopyright_conn = null
-          }).catch(console.error)
-        })
-      }).catch(console.error)
-    }
-  } else if(old.channelID == CHID_NOCOPYRIGHT && cur.channelID != CHID_NOCOPYRIGHT) {
-    // user exited Chillstep --decrement count
-    if(--ZJNoCopyright_count < 0)
-      ZJNoCopyright_count = 0
-    // check for lonely bot
-    if(ZJNoCopyright_count <= 1) {
-      if(ZJNoCopyright_conn) {
-        // remove chillstep bot
-        ZJNoCopyright_conn.disconnect()
-        ZJNoCopyright_conn = null
-      }
-    }
-  }
-})
 
 // clean shutdown and restart on SIGTERM
 process.once('SIGTERM', () => {
@@ -314,13 +193,18 @@ process.once('SIGTERM', () => {
   Zyborg.channels.fetch(CHID_SPAM).then(channel => {
     channel.send("```SIGTERM detected. Restarting...```").catch(console.error)
   }).catch(console.error)
+  
   // destroy all bots
   Zyborg.destroy()
-  ZJChillstep.destroy()
-  ZJNoCopyright.destroy()
+  ZJ_Chillstep.destroy()
+  ZJ_NCM.destroy()
+  ZJ_NCS.destroy()
   
   process.exit(101)
 })
 
 // bot logins
 Zyborg.login(process.env.ZYBORG_TOKEN)
+ZJ_Chillstep.login(process.env.ZJCHILLSTEP_TOKEN)
+ZJ_NCM.login(process.env.ZJNOCOPYRIGHTMUSIC_TOKEN)
+ZJ_NCS.login(process.env.ZJNOCOPYRIGHTSOUNDS_TOKEN)
---
+++ musicbot.js
@@ -0,0 +1,70 @@
+/* eslint-env node, es6 */
+/* eslint-disable no-console */
+
+const ytdl = require("ytdl-core")
+const Discord = require("discord")
+
+const VOLUME = 0.2
+
+exports.module = function(n, id, src) {
+  // make 'this' reliably accessible
+  const _self = this
+  
+  // define options and parameters
+  _self.name = n
+  _self.chid = id
+  _self.source = src
+  _self.conn = null
+  _self.count = 0
+  // define the client
+  _self.client = new Discord.Client()
+  
+  // setup events for the client...
+  /* ...on ready, log event and post stream link */
+  _self.client.on("ready", () => {
+    console.log(`${_self.client.user.tag} is ready...`)
+  })
+  /* ...on voiceStateUpdate, check user joined before starting */
+  _self.client.on("voiceStateUpdate", (old, cur) => {
+    if(old.channelID != _self.chid && cur.channelID == _self.chid)
+    {
+      // user joined Chillstep ++increment count
+      if(++_self.count == 1) {
+        // start chillstep bot
+        cur.member.voice.channel.join().then(connection => {
+          _self.conn = connection
+          connection.play(
+            ytdl(_self.source, {quality:'highestaudio'}),
+            {volume: VOLUME}
+          ).on("error", error => {
+            console.error(`${_self.name}: ${error}`)
+            connection.disconnect()
+            _self.conn = null
+          })
+        }).catch(console.error)
+      }
+    }
+    else if(old.channelID == _self.chid && cur.channelID != _self.chid)
+    {
+      // user exited Chillstep --decrement count
+      if(--_self.count < 0)
+        _self.count = 0
+      // check for lonely bot
+      if(_self.count <= 1) {
+        if(_self.conn) {
+          // remove chillstep bot
+          _self.conn.disconnect()
+          _self.conn = null
+        }
+      }
+    }
+  })
+  
+  _self.destroy = function() {
+    _self.client.destroy()
+  }
+  
+  _self.login = function(token) {
+    _self.client.login(token)
+  }
+}
